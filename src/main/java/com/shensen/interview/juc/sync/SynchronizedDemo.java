84c49c4b778d1397675e29913a18d2a33d947333
Alwyn <nowtry@yeah.net>
2020-07-16 22:16:30 +0800

    commit synchronized demo

--- src/main/java/com/shensen/interview/juc/sync/SynchronizedDemo.java
+++ src/main/java/com/shensen/interview/juc/sync/SynchronizedDemo.java
@@ -2,9 +2,13 @@ package com.shensen.interview.juc.sync;
 
 /**
  * <p>
- * Synchronized关键字.
- * 1、使用方式：1、修饰实例方法；2、修饰静态方法；3、修饰代码块
- * 2、利用 javap -c -s -v -l 命令查看底层原理.
+ * synchronized可以保证方法或者代码块在运行时，同一时刻只有一个方法可以进入到临界区，同时它还可以保证共享变量的内存可见性
+ * 使用对象：
+ * 1、普通同步方法，锁是当前实例对象；
+ * 2、静态同步方法，锁是当前类的class对象；
+ * 3、同步方法块，锁是括号里面的对象
+ *
+ * 利用 javap -c -s -v -l 命令查看底层原理.
  * </p>
  *
  * @author Alwyn
@@ -12,19 +16,23 @@ package com.shensen.interview.juc.sync;
  */
 public class SynchronizedDemo {
 
-    public void methodA() {
-        synchronized (this) {
-            System.out.println("synchronized 代码块.");
-        }
+    public synchronized void methodA() {
+        System.out.println("synchronized 普通同步方法.");
     }
 
-    public synchronized void methodB() {
-        System.out.println("synchronized 方法.");
+    public synchronized static void methodB() {
+        System.out.println("synchronized 静态同步方法.");
     }
 
     public void methodC() {
+        synchronized (this) {
+            System.out.println("synchronized 同步方法块.");
+        }
+    }
+
+    public void methodD() {
         synchronized (SynchronizedDemo.class) {
-            System.out.println("synchronized 代码块.");
+            System.out.println("synchronized 同步方法块.");
         }
     }
 }
