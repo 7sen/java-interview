a5d77daf3cf7d3b983901a0f2c5ced832e9221f4
alwyn <nowtry@yeah.net>
2021-02-23 15:54:24 +0800

    Fixed known issues.

--- src/main/java/com/shensen/interview/juc/sync/SynchronizedDemo.java
+++ src/main/java/com/shensen/interview/juc/sync/SynchronizedDemo.java
@@ -6,7 +6,7 @@ package com.shensen.interview.juc.sync;
  * 使用对象：
  * 1、普通同步方法，锁是当前实例对象；
  * 2、静态同步方法，锁是当前类的class对象；
- * 3、同步方法块，锁是括号里面的对象
+ * 3、同步代码块，锁是括号里面的对象（monitorenter、monitorexit）
  *
  * 利用 javap -c -s -v -l 命令查看底层原理.
  * </p>
@@ -16,23 +16,23 @@ package com.shensen.interview.juc.sync;
  */
 public class SynchronizedDemo {
 
-    public synchronized void methodA() {
+    public synchronized void method1() {
         System.out.println("synchronized 普通同步方法.");
     }
 
-    public synchronized static void methodB() {
+    public synchronized static void method2() {
         System.out.println("synchronized 静态同步方法.");
     }
 
-    public void methodC() {
+    public void method3() {
         synchronized (this) {
-            System.out.println("synchronized 同步方法块.");
+            System.out.println("synchronized 同步代码块.");
         }
     }
 
-    public void methodD() {
+    public void method4() {
         synchronized (SynchronizedDemo.class) {
-            System.out.println("synchronized 同步方法块.");
+            System.out.println("synchronized 同步代码块.");
         }
     }
 }
